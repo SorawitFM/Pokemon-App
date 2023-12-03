4f2829a4a6fec32725840f1f8dcfcc43d30dcff5
SorawitFM <fmzazadds2@gmail.com>
2023-12-04 00:17:14 +0700

    Revert "Revert "commit 22 DropDown Generation เสร็จ""
    This reverts commit 460d38b698f16c3dde427dc68c2e579fca4d4b5e.

--- src/components/SearchForm/SearchForm.hook.ts
+++ src/components/SearchForm/SearchForm.hook.ts
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { pokemonListServie, pokemonDetailServie } from '@/service'
 import { useForm } from "react-hook-form"
 import { usePokemonListStore } from '@/store/pokemonList'
+import { generationList } from '@/utils/optionList';
 
 const useSearchForm = () => {
     const { register, handleSubmit, watch, formState: { errors }, } = useForm() //เป็น pattern ของ library "react-hook-form"
@@ -10,9 +11,16 @@ const useSearchForm = () => {
     const { setFetchPokemonList, fetchPokemon, setPokemonList } = usePokemonListStore()//เป็น pattern ของ library "react-hook-form"
 
     const keyword = watch('keyword') //เป็น pattern ของ library "react-hook-form"
+    const generation = watch('generation') //เป็น pattern ของ library "react-hook-form"
+    const type = watch('type') //เป็น pattern ของ library "react-hook-form"
+    const sort = watch('sort') //เป็น pattern ของ library "react-hook-form"
 
-    const callData = async () => {
-        const responseList = await pokemonListServie.getPokemonList()
+    const callData = async (filter: {
+        name: string
+        limit: number
+        offset: number
+    }) => {
+        const responseList = await pokemonListServie.getPokemonList(filter.limit, filter.offset)
         if (responseList.status === 200) {
             const responseResult = responseList.data?.results || []
             const pokeList = []
@@ -40,8 +48,11 @@ const useSearchForm = () => {
     }
 
     useEffect(() => {
-        callData()
-    }, [])
+        if (generation !== undefined) {
+            callData(generationList[generation])
+        }
+
+    }, [generation])
 
     useEffect(() => {
         const data = fetchPokemon.data.filter((Item) => {
@@ -56,7 +67,10 @@ const useSearchForm = () => {
 
 
     return {
-        fieldKeyword: register('keyword') //เป็น pattern ของ library "react-hook-form"
+        fieldKeyword: register('keyword'), //เป็น pattern ของ library "react-hook-form"
+        fieldGeneration: register('generation'), //เป็น pattern ของ library "react-hook-form"
+        fieldType: register('type'), //เป็น pattern ของ library "react-hook-form"
+        fieldSort: register('sort') //เป็น pattern ของ library "react-hook-form"
     }
 }
 
